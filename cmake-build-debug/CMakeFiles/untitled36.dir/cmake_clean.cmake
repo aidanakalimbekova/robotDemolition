52382ea43db80ab10db9f359a617bc2cf5142e09
aidanakalimbekova <58738984+aidanakalimbekova@users.noreply.github.com>
2021-09-20 23:43:52 +0600

    add files

---
+++ cmake-build-debug/CMakeFiles/untitled36.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled36.dir/main.c.obj"
+  "untitled36.exe"
+  "untitled36.exe.manifest"
+  "untitled36.lib"
+  "untitled36.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled36.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
